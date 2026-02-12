617c29f3af959fdfcc2bc36414b11663812d05f7
Pratham Gupta <ufuxl@student.kit.edu>
2026-02-13 00:14:27 +0100

    update

--- script.js
+++ script.js
@@ -54,12 +54,12 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
+    "Dekhle",
+    "Bas karja??",
+    "Rulaegi kya?",
+    "maanjaoo jaaneman",
+    "yes par click kr chup chaap se",
+    "If you say no, mai khaana nhin banaunga",
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
